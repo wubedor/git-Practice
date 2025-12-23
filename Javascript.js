cd94b81a6e700808b9699678ce9332142b5145b2
wubedor <anumemma24@gmail.com>
2025-12-23 02:32:52 +0000

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,154 @@
+
+//string
+var name= 'Hello'
+var name= "This is Correct"
+var name= 'Day is bright'
+var name= "What is your name"
+var name= "kojo"
+
+//integers
+let weight= 18
+let result= -6
+let rest= -8
+let grow= 68
+let size= 7
+
+//float
+let power= 45.8
+let voltage= -7.9
+let height= 67.2
+var volt= -17.7
+var width= 97.1
+
+//boolean
+var adult= true 
+var adult= false 
+let child= true 
+let mother= true
+var baby= false 
+
+console.log (power/height)
+console.log(voltage*width)
+console.log(size+volt)
+console.log(rest-voltage)
+
+//document write
+document.write(name)
+document.write(age)
+document.write(adult)
+document.write(power)
+document.write(rest)
+document.write(volt)
+document.write(mother)
+document.write(child)
+document.write(height)
+document.write(baby)
+
+
+
+
+
+
+
+
+
+var age= 19
+var adult= true
+
+console.log(name)
+console.log(age)
+ console.log(adult)
+
+name='Hello Guy'
+
+console.log(name)
+
+
+/*let surname= 'Emmanuel Sowah'
+let gen = 75 
+let boy = true
+ 
+boy= false
+
+console.log(surname)
+console.log(gen)
+console.log(boy)*/
+
+const homeTown= 'Accra'
+const streetNo= 74
+const resident= true
+
+console.log(homeTown)
+console.log(streetNo)
+console.log(resident)
+
+let goods= 700
+let books= 1790
+
+console.log(goods + books)
+
+
+let mats= 50
+let shoe= 35
+
+console.log(mats - shoe)
+
+let cook= 40
+let cold= 10
+
+console.log(cook * cold)
+
+let coast= 50
+let bike= 5
+
+console.log(coast / bike)
+
+let pen= 105
+let pencil= 50
+
+console.log(pen % pencil)
+
+let cool= 10
+let hot= 2
+
+console.log(++cool)
+console.log(++hot)
+
+let table= 10
+let chair= 2
+
+console.log(table++)
+console.log(table)
+
+console.log(chair++)
+console.log(chair)
+
+let card= 10
+let broom= 2
+
+console.log(--card)
+console.log(--broom)
+
+let eye= 20
+let noise= 10
+
+console.log(eye--)
+console.log(eye)
+
+console.log(noise--)
+console.log(noise)
+
+let numberofRats= 19
+let numberofBats= 30
+
+console.log(numberofRats == numberofBats)
+
+let numberofRat= 19
+let numberofBat= 19
+
+console.log(numberofRat == numberofBat)
+
+let numberofBoot= 19
+let numberofBoat= 19
+
+console.log(numberofBoot === numberofBoat )
